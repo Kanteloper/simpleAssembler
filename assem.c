c3995210c6bff5261d170265335e02887641ec75
kanteloper <com4943@gmail.com>
2018-07-04 23:11:49 +0900

    first pass is in process

--- assem.c
+++ assem.c
@@ -1,9 +1,18 @@
+/*
+ * Date : 2018-07-04
+ * Name : Kanteloper
+ * Detail : implement first pass of assembler
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
+#define SYM_MAX 30
 
 int main(int argc, char** argv)
 {
 	FILE *fp;
+	int lc = 0; // location counter
+	char** symTab[SYM_MAX]; // symbol table
 
 	// check parameter
 	if(argc < 2) 
@@ -19,6 +28,15 @@ int main(int argc, char** argv)
 		exit(1);
 	}
 
-	puts("success to open a file");
+	// start first pass
+	while(1)
+	{
+		
+	}
+
+	// check that there is a symbol in label field
+	// store symbol in symbol table
+	
+
 	
 }
